78fb79a53c5e7cc214b9e87a990ff24bc0fc6cf3
386083520 <386083520@qq.com>
2022-02-21 16:08:32 +0800

    state的获取及其响应式

--- dist/vuex.js
+++ dist/vuex.js
@@ -27,12 +27,26 @@
     class Store {
         constructor (options = {}) {
             console.log('gsdoptions', options);
+            const state = options.state;
+            resetStoreVM(this, state);
+        }
+        get state () {
+            return this._vm._data.$$state
         }
     }
 
     function install (_Vue) {
         Vue = _Vue;
         applyMixin(Vue);
+    }
+
+    function resetStoreVM (store, state, hot) {
+        store._vm = new Vue({
+            data: {
+                $$state: state
+            }
+        });
+        console.log('gsdstore_vm', store._vm);
     }
 
     var index = {
--- src/store.js
+++ src/store.js
@@ -5,10 +5,24 @@ let Vue
 export class Store {
     constructor (options = {}) {
         console.log('gsdoptions', options)
+        const state = options.state
+        resetStoreVM(this, state)
+    }
+    get state () {
+        return this._vm._data.$$state
     }
 }
 
 export function install (_Vue) {
     Vue = _Vue
     applyMixin(Vue)
+}
+
+function resetStoreVM (store, state, hot) {
+    store._vm = new Vue({
+        data: {
+            $$state: state
+        }
+    })
+    console.log('gsdstore_vm', store._vm)
 }
